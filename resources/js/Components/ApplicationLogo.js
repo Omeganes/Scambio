9b7879a08d75d86141ac6c710361eaab845f15a4
Raymond Youssef <rimonomega@gmail.com>
2021-05-27 23:23:59 +0200

    initialized auth scafolding

---
+++ resources/js/Components/ApplicationLogo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function ApplicationLogo({ className }) {
+    return (
+        <img src={'logo.png'} className={className} alt={'application logo'}/>
+    );
+}
