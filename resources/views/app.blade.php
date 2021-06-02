58bda09ebf2a1b39347a54db62c287eab7214ec4
Raymond Youssef <rimonomega@gmail.com>
2021-06-02 16:42:51 +0200

    Finish Home page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,11 +9,11 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&family=Reggae+One&display=swap" rel="stylesheet">
-        {{--TODO--}}
-        <script src="https://kit.fontawesome.com/d1b32dee55.js" crossorigin="anonymous"></script>
+
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.min.css') }}">
         <link rel="stylesheet" href="{{ mix('css/custom.min.css') }}">
+
         <!-- Scripts -->
         @routes
         <script src="{{ mix('js/app.js') }}" defer></script>
