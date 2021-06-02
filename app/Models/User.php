d38359d02a3eae9f61ac72bf403c365705e85e11
Raymond Youssef <rimonomega@gmail.com>
2021-06-02 16:31:08 +0200

    Add Category Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone',
+        'exchanges_count',
+        'account_number'
     ];
 
     /**
